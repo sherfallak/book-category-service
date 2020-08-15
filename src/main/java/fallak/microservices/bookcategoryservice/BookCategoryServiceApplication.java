1cc3eb9e0d46bf90821eae2824d5cda9a15453e8
sherfallak <sh.fallak@gmail.com>
2020-08-15 18:20:16 +0200

    initial commit

---
+++ src/main/java/fallak/microservices/bookcategoryservice/BookCategoryServiceApplication.java
@@ -0,0 +1,13 @@
+package fallak.microservices.bookcategoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCategoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCategoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fallak/microservices/bookcategoryservice/BookCategoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fallak.microservices.bookcategoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCategoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
